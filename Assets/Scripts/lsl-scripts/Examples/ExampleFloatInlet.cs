79b89e9e7b10ed4ceef0e43cc8784ba1fb188f2b
Athanasios (Thanos) Vourvopoulos <a.vourvopoulos@gmail.com>
2020-05-29 13:53:13 +0100

    removed debug print

--- Assets/Scripts/lsl-scripts/Examples/ExampleFloatInlet.cs
+++ Assets/Scripts/lsl-scripts/Examples/ExampleFloatInlet.cs
@@ -18,9 +18,7 @@ namespace Assets.LSL4Unity.Scripts.Examples
             // just as an example, make a string out of all channel values of this sample
             lastSample = string.Join(" ", newSample.Select(c => c.ToString()).ToArray());
 
-            Debug.Log(
-                string.Format("Got {0} samples at {1}", newSample.Length, timeStamp)
-                );
+            //Debug.Log(string.Format("Got {0} samples at {1}", newSample.Length, timeStamp));
 
             if (lastSample != String.Empty)
             {
